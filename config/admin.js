454beff11bd315cdb8bee2d93e74106b4311d490
viral <viralpatel.patel78@gmail.com>
2018-06-19 19:26:44 +0530

    first commit

---
+++ config/admin.js
@@ -0,0 +1,8 @@
+// config used by dashboard client side only
+module.exports = {
+  // dashboard UI language
+  language: 'en',
+  apiBaseUrl: 'http://206.189.132.182:3001/api/v1',
+  apiWebSocketUrl: 'ws://206.189.132.182:3001',
+  developerMode: false
+}
---
+++ config/store.js
@@ -0,0 +1,6 @@
+// config used by store client side only
+module.exports = {
+  // store UI language
+  language: 'en',
+  ajaxBaseUrl: 'http://206.189.132.182:3001/ajax'
+}
